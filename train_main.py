78db77adfd8d13ba65e9ed8fc68e978797bcf984
滝沢 <takizawa@liberteenz.jp>
2021-01-27 12:48:57 +0900

    推論で使うものを追加

--- train_main.py
+++ train_main.py
@@ -95,6 +95,20 @@ def get_valid_transforms():
             ToTensorV2(p=1.0),
         ], p=1.)
 
+#推論で使うもの(こちらのほうが、get_valid_transformsよりもスコアが0.005も高い)
+#https://www.kaggle.com/takiyu/cassava-resnext50-32x4d-inference?scriptVersionId=52803745
+def get_inference_transforms():
+    return Compose([
+            RandomResizedCrop(CFG['img_size'], CFG['img_size']),
+            Transpose(p=0.5),
+            HorizontalFlip(p=0.5),
+            VerticalFlip(p=0.5),
+            HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
+            RandomBrightnessContrast(brightness_limit=(-0.1,0.1), contrast_limit=(-0.1, 0.1), p=0.5),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
+            ToTensorV2(p=1.0),
+        ], p=1.)
+        
 
 
 if __name__ == '__main__':
