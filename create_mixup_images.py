38556786c870074a616dfb1d2272d0be98c72629
taki mac <takizawa@liberteenz.jp>
2021-02-03 09:00:31 +0900

    image作成

--- create_mixup_images.py
+++ create_mixup_images.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import shutil
 import keras
+from tqdm import tqdm
+import random
 
 #mixup後の画像を格納するフォルダ名
 folder = "./mixup_alpha_1/"
@@ -21,7 +23,7 @@ end_count = 10 #画像はbatch_size * end_count分作成される
 num_classes = 5
 
 #csvファイルを読み込む
-train = pd.read_csv('../input/cassava-leaf-disease-classification/train.csv' , nrows = 120)
+train = pd.read_csv('../input/cassava-leaf-disease-classification/train.csv' , nrows = 12000)
 
 data_root = '../input/cassava-leaf-disease-classification/train_images/'
 
@@ -34,57 +36,92 @@ noisy_label = pd.read_csv("./src/data/noisy_label.csv")
 train["label"] = noisy_label["guess_label"]
 print("train label clean change")
 
-train_X =[]
-train_y = []
-for index, row in train.iterrows():
-    file_name = row['image_id']
-    file_path = f'{data_root}/{file_name}'
-    image = cv2.imread(file_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    train_X.append(image)
-    train_y.append(row['label'])
+
+
+def get_mixup_data(train, label_id):
+
+    train_X =[]
+    train_y = []
+    j_count = 0
+    for index, row in tqdm(train.iterrows()):
+        file_name = row['image_id']
+        file_path = f'{data_root}/{file_name}'
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        train_X.append(image)
+
+        #擬似的にランダムなラベルを設定する
+        train_y.append(random.randrange(num_classes))
+        
+    #numpyに変換する
+    train_X = np.array(train_X)
+    train_y = np.array(train_y)
+    train_y = keras.utils.to_categorical(train_y, num_classes)
+
+    #MixUPを実行する
+    generator = MixupGenerator(train_X, train_y, alpha=alpha, batch_size=batch_size )()
+
+
+    #画像作成したら随時格納
+    images_names = []
+    label_names = []
     
-#numpyに変換する
-train_X = np.array(train_X)
-train_y = np.array(train_y)
-train_y = keras.utils.to_categorical(train_y, num_classes)
+    for batch_index, (x, y) in enumerate(generator):
+        if(end_count < batch_index):
+            break
 
+        y = np.argmax(y,  axis = 1)
+        for index in range(0, len(y)):
 
+            #画像作成する
+            j_name = "mix_1_" +  str(label_id) + "_"+ str(j_count) + ".jpg"
+            path = folder + j_name
 
-#MixUPを実行する
-generator = MixupGenerator(train_X, train_y, alpha=alpha, batch_size=batch_size )()
+            cv2.imwrite(path, x[index])
 
-#画像作成したら随時格納
-images_names = []
-label_names = []
-j_count = 0
-for batch_index, (x, y) in enumerate(generator):
-    if(end_count < batch_index):
-        break
+            #画像ファイルとラベルを作る
+            images_names.append(j_name)
+            label_names.append(y[index])
 
-    y = np.argmax(y,  axis = 1)
-    for index in range(0, len(y)):
+            j_count +=1
 
-        #画像作成する
-        j_name = "mix_1_" + str(j_count) + ".jpg"
-        path = folder + j_name
+    #dfデータ作成
+    df = pd.DataFrame()
+    df["image_id"] = images_names
+    df["label"] = label_id
 
-        cv2.imwrite(path, x[index])
+    return df
 
-        #画像ファイルとラベルを作る
-        images_names.append(j_name)
-        label_names.append(y[index])
+#画像を作成する
+df_0 = train[train['label'] == 0][0:2000]
+df_0 = get_mixup_data(df_0, 0)
 
-        j_count +=1
+df_1 = train[train['label'] == 1][0:1400]
+df_1 = get_mixup_data(df_1, 1)
+
+df_2 = train[train['label'] == 2][0:1400]
+df_2 = get_mixup_data(df_2, 2)
+
+df_3 = train[train['label'] == 3][0:100]
+df_3 = get_mixup_data(df_3, 3)
+
+df_4 = train[train['label'] == 4][0:1400]
+df_4 = get_mixup_data(df_4, 4)
+
+df = pd.concat([df_0, df_1, df_2, df_3, df_4])
+df = df.reset_index(drop=True)
+
+#dfファイル作成
+#df = pd.DataFrame()
+#df["image_id"] = images_names
+#df["label"] = label_names
+df.to_csv("mix_train.csv", index = False)
+
 
 #フォルダを圧縮
 shutil.make_archive(folder, 'zip', root_dir=folder)
 
-#dfファイル作成
-df = pd.DataFrame()
-df["image_id"] = images_names
-df["label"] = label_names
-df.to_csv("mix_train.csv", index = False)
+
 
 #元のフォルダ削除
 shutil.rmtree(folder)
